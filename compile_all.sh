700f698ca4e514346029c872a824eabb9f2880a0
hactrox <hactrox@gmail.com>
2021-07-22 18:39:28 +0800

    improve: optimize compiler shell output format

--- compile_all.sh
+++ compile_all.sh
@@ -10,6 +10,6 @@ declare -a contracts=(
 for contract in ${contracts[@]}; do
     solPath="./contracts/"
     solPath+=$contract
-    printf "%-26s " "$contract"
+    printf "%-34s " "$contract"
     solc $solPath --bin --abi --evm-version istanbul --optimize --optimize-runs 200 -o ./build/contracts --allow-paths . --overwrite
 done
